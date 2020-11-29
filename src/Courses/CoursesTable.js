df1e2b50afcad8c4233634e26fadde36618d0fa3
angelos.ar <angelos.aramp@gmail.com>
2020-11-29 09:57:20 +0200

    refactor(CourseTable): move to Courses

---
+++ src/Courses/CoursesTable.js
@@ -0,0 +1,70 @@
+import { Table, Tag, Space, Button, Spin } from 'antd';
+import { CheckOutlined } from '@ant-design/icons';
+import useCourses from "./useCourses";
+
+const columns = [
+  {
+    title: 'Title',
+    dataIndex: 'title',
+    key: 'title',
+    render: text => <a>{text}</a>,
+    width: '30%'
+  },
+  {
+    title: 'Bookable',
+    dataIndex: 'open',
+    key: 'open',
+    align: 'center',
+    render: (open) => (open ? <CheckOutlined style={{color:"green"}} /> : 'No')
+  },
+  {
+    title: 'Price',
+    dataIndex: ['price','normal'],
+    key: 'price',
+  },
+  {
+    title: 'Date',
+    key: 'dates',
+    dataIndex: 'dates',
+    width: '30%',
+    render: dates => (
+      <>
+        {dates.start_date} - {dates.end_date} 
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+        <Button type="primary">TODO:View details</Button>
+    ),
+  },
+];
+
+const CoursesTable = () => {
+  const [courses, isLoading] = useCourses({
+    url: "http://localhost:3001",
+    format: "json",
+    resource: "courses"
+  });
+
+    return (
+      <>
+        {isLoading ? (
+          <Spin size="large" />
+        ) : (
+        <Table 
+          className="courses-table" 
+          title={() => {
+            return (<h3><strong>Five newest courses</strong></h3>)
+          }} 
+          columns={columns} 
+          dataSource={courses}
+          pagination={false} /> 
+        )}
+      </>
+    );
+}
+
+export default CoursesTable;
